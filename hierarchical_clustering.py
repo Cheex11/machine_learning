0c2451bbe0846e3f10fe3f1392c9a81112d2d74e
Cody Wilson <Cheex11@gmail.com>
2015-05-20 00:12:54 -0500

    working on adding data for clustering

--- hierarchical_clustering.py
+++ hierarchical_clustering.py
@@ -3,11 +3,11 @@ from sklearn.cluster import MeanShift
 from sklearn.datasets.samples_generator import make_blobs
 import matplotlib.pyplot as plt
 
-centers = [[1,1],[5,5]]
+centers = [[0,20],[200,20],[400,20],[600,20]]
 
 # "_" is not labeled.  It is "y"
 #MeanShift is accurate up to 10,000
-X, _ = make_blobs(n_samples = 200, centers = centers, cluster_std = 1)
+X, _ = make_blobs(n_samples = 10, centers = centers, cluster_std = .01)
 
 plt.scatter(X[:,0], X[:,1])
 plt.show()
@@ -21,6 +21,8 @@ labels = ms.labels_
 #These are the estimated centers.  The higher the samples and lowe the std, the more accurate this approximation will be
 cluster_centers = ms.cluster_centers_
 
+print(cluster_centers)
+
 #Tells you how many unique variables exist.  It will tell you how many categories the machine has found
 n_clusters_ = len(np.unique(labels))
 
@@ -32,6 +34,7 @@ colors = 10*['r.','g.','b.','c,','k,','y,','m.']
 print(colors)
 print(labels)
 
+#Plot the data!
 for i in range(len(X)):
     plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize = 10)
 
