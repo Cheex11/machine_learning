66bb0412c32e1025d95f56ba7a76db1431c64266
Cody Wilson <Cheex11@gmail.com>
2015-05-19 23:36:00 -0500

    hierarchical clustering now working

--- hierarchical_clustering.py
+++ hierarchical_clustering.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.cluster import MeanShift as ms
+from sklearn.cluster import MeanShift
 from sklearn.datasets.samples_generator import make_blobs
 import matplotlib.pyplot as plt
 
@@ -11,6 +11,8 @@ X, _ = make_blobs(n_samples = 200, centers = centers, cluster_std = 1)
 
 plt.scatter(X[:,0], X[:,1])
 plt.show()
+
+ms = MeanShift()
 
 #Fit a capital X at the fit
 ms.fit(X)
@@ -30,4 +32,11 @@ colors = 10*['r.','g.','b.','c,','k,','y,','m.']
 print(colors)
 print(labels)
 
+for i in range(len(X)):
+    plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize = 10)
+
+plt.scatter(cluster_centers[:,0], cluster_centers[:,1],
+            marker="x", s=150, linewidths = 5, zorder=10)
+
+plt.show()
 
